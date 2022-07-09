e8037e4e09f40a8a7617d9b424a59c64d4da1891
nguyen.dao1 <nguyen.dao1@tiki.vn>
2022-07-09 11:52:01 +0700

    init

---
+++ backend/api/crud.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+class CRUD():
+	def __init__(self):
+		pass
+
+	def create(self):
+		pass
+
+	def remove(self):
+		pass
+
+	def update(self):
+		pass
+
+	def delete(self):
+		pass
+
