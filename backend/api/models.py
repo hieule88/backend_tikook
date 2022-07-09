b9715f9d46ac966269c6db75c98893bef130a62f
HIEU LE DINH <HIEU.LD183530@sis.hust.edu.vn>
2022-07-09 16:15:59 +0700

    tikingonitem

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
+from xml.etree.ElementTree import Comment
 from django.db import models
 
 # Create your models here.
+class TikiNgonItem(models.Model):
+    ItemID = models.AutoField(primary_key=True)
+    Name = models.CharField()
+    Price = models.FloatField()
+    Discount = models.IntegerField()
+    Stars = models.FloatField()
+    NumStarts = models.IntegerField()
+    ImageItem = models.ImageField()
+    NumBuys = models.IntegerField()
+    InStock = models.IntegerField()
+    Provider = models.CharField()
+    Amount = models.IntegerField()
+    Description = models.CharField()
